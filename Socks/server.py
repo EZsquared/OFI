d7f89179a6bf9208e496c020a3cf2348bb013ba5
EasiestE <ericcannon01@gmail.com>
2020-05-27 11:15:04 -0400

    Working on socket programming

--- Socks/server.py
+++ Socks/server.py
@@ -1,3 +1,18 @@
 import socket
 
-s = socket.socket( )
+s = socket.socket()
+print('Socket Created')
+
+s.bind(('localhost', 9999))
+
+s.listen(3)
+print('waiting for connections')
+
+while True:
+    c, addr = s.accept()
+    print('connected with ', addr)
+    name = c.recv(1024).decode()
+    
+    c.send(bytes('Welcome to OFI', 'utf-8'))
+
+    c.close()
